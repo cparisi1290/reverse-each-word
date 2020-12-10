9db239133997ce687f77b4392e234099b332d885
Cassandra <cparisi1290@gmail.com>
2020-12-10 15:12:21 -0500

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(string)
+    string_array = string.split(" ")
+    #reversed_array = []
+    #string_array.each do |word|
+        #reversed_array << word.reverse
+    #end
+    #reversed_array.join(" ")
+#end
+    collect_array = string_array.collect do |word|
+        word.reverse
+    end 
+    collect_array.join(" ")
+end
